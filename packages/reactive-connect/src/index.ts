9aaf17c419002296767c6afd5f0a7b4ec78cb583
yibuyisheng <yibuyisheng@163.com>
2019-10-30 14:22:56 +0800

    feat: update configs and exports some modules

---
+++ packages/reactive-connect/src/index.ts
@@ -0,0 +1,15 @@
+export * from './decorators/index';
+
+export { default as AppStore } from './AppStore';
+
+export { default as ComponentStore } from './ComponentStore';
+
+export { default as PageStore } from './PageStore';
+
+export { default as Store } from './Store';
+
+export { default as createMiniApp } from './createMiniApp';
+
+export { default as createMiniComponent } from './createMiniComponent';
+
+export { default as createMiniPage } from './createMiniPage';
