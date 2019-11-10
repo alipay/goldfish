f431c901050c3fbdb01041549e504b3232810594
yibuyisheng <yibuyisheng@163.com>
2019-11-10 20:58:49 +0800

    feat: add feedback

---
+++ packages/framework/src/plugins/feedback/index.ts
@@ -0,0 +1,2 @@
+export { default as FeedbackPlugin } from './FeedbackPlugin';
+export * from './FeedbackPlugin';
--- packages/framework/src/store/AppStore.ts
+++ packages/framework/src/store/AppStore.ts
@@ -1,11 +1,21 @@
 import { AppStore as BaseAppStore } from '@alipay/goldfish-reactive-connect';
 import { PluginHub, PluginClass } from '@alipay/goldfish-plugins';
 import { IConfig, default as ConfigPlugin } from '../plugins/ConfigPlugin';
+import FeedbackPlugin, {
+  IToastOption,
+  IAlertOption,
+  IConfirmOption,
+  IPromptOption,
+  FeedbackOption,
+} from '../plugins/feedback/FeedbackPlugin';
+import { asyncForEach } from '@alipay/goldfish-utils';
 
 export default class AppStore extends BaseAppStore {
   protected pluginHub: PluginHub = new PluginHub();
 
   protected config?: IConfig;
+
+  protected stopWatchFeedbackQueue: (() => void) | undefined;
 
   /**
    * Set the config immediately after the AppStore is created.
@@ -26,6 +36,7 @@ export default class AppStore extends BaseAppStore {
   protected getPlugins(): PluginClass[] {
     return [
       ConfigPlugin,
+      FeedbackPlugin,
     ];
   }
 
@@ -104,5 +115,160 @@ export default class AppStore extends BaseAppStore {
   public destroy() {
     super.destroy();
     this.pluginHub.destroy();
+  }
+
+  public startWatchFeedbackQueue(options: {
+    showToast?: (item: IToastOption) => Promise<void>;
+    alert?: (item: IAlertOption) => Promise<void>;
+    confirm?: (item: IConfirmOption) => Promise<void>;
+    prompt?: (item: IPromptOption) => Promise<void>;
+  }) {
+    const feedback = this.pluginHub.get(FeedbackPlugin);
+    if (!feedback) {
+      throw new Error('No feedback.');
+    }
+
+    if (this.stopWatchFeedbackQueue) {
+      return this.stopWatchFeedbackQueue;
+    }
+
+    let isFeedbackConsuming = false;
+    let shouldStopIteration = false;
+    const stop = this.watch(
+      () => feedback.feedbackQueue,
+      async () => {
+        if (isFeedbackConsuming) {
+          return;
+        }
+        isFeedbackConsuming = true;
+
+        const queue = [...feedback.feedbackQueue];
+        await asyncForEach(queue, async (item: FeedbackOption) => {
+          if (shouldStopIteration) {
+            return true;
+          }
+
+          switch (item.popType) {
+            case 'toast':
+              await (options.showToast && options.showToast(item));
+              break;
+            case 'alert':
+              await (options.alert && options.alert(item));
+              break;
+            case 'confirm':
+              await (options.confirm && options.confirm(item));
+              break;
+            case 'prompt':
+              await (options.prompt && options.prompt(item));
+              break;
+          }
+        });
+
+        isFeedbackConsuming = false;
+
+        if (feedback.feedbackQueue.length && queue.length) {
+          feedback.feedbackQueue.splice(0, queue.length);
+        }
+      },
+      {
+        immediate: true,
+      },
+    );
+    this.stopWatchFeedbackQueue = () => {
+      stop();
+      this.stopWatchFeedbackQueue = undefined;
+      shouldStopIteration = true;
+    };
+    return this.stopWatchFeedbackQueue;
+  }
+
+  public async initFeedback() {
+    await this.waitForPluginsReady();
+
+    this.startWatchFeedbackQueue({
+      showToast: (item): Promise<void> => {
+        return new Promise((resolve) => {
+          my.showToast(item);
+          if (item.isBlock || item.duration) {
+            if (item.duration) {
+              setTimeout(() => resolve(), item.duration);
+            } else {
+              resolve();
+              my.hideToast();
+            }
+          } else {
+            my.hideToast();
+            resolve();
+          }
+        });
+      },
+      alert: (item) => {
+        return new Promise((resolve) => {
+          if (item.isBlock) {
+            my.alert({
+              ...item,
+              complete: () => {
+                resolve();
+                if (item.complete) {
+                  item.complete();
+                }
+              },
+            });
+          } else {
+            my.alert(item);
+            resolve();
+          }
+        });
+      },
+      confirm: (item) => {
+        return new Promise((resolve) => {
+          my.confirm({
+            ...item,
+            confirmButtonText: item.okButtonText,
+            complete: (result) => {
+              if (item.isBlock) {
+                resolve();
+              }
+              if (item.complete) {
+                item.complete(
+                  result.confirm
+                    ? { ok: true, cancel: false }
+                    : { ok: false, cancel: true },
+                );
+              }
+            },
+          });
+          if (!item.isBlock) {
+            resolve();
+          }
+        });
+      },
+      prompt: (item) => {
+        return new Promise((resolve) => {
+          my.prompt({
+            ...item,
+            message: item.content || 'prompt',
+            success: (result) => {
+              item.complete && item.complete(
+                result.ok
+                  ? {
+                    ok: true,
+                    cancel: false,
+                    inputValue: result.inputValue,
+                  }
+                  : {
+                    ok: false,
+                    cancel: true,
+                  },
+              );
+            },
+            fail: () => {
+              item.complete && item.complete({ ok: false, cancel: false });
+            },
+            complete: resolve,
+          });
+        });
+      },
+    });
   }
 }
