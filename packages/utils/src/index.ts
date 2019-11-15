c7361e8266c34a25e40c002f92c321f8b6c5464d
likun7981 <likun7981@gmail.com>
2019-11-15 16:27:06 +0800

    feat: Add parseUrl to utils

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,6 +1,6 @@
 export { default as cache } from './cache';
 export { default as takeLatest } from './takeLatest';
-// export { default as parseUrl } from './parseUrl';
+export { default as parseUrl } from './parseUrl';
 export { default as asyncForEach } from './asyncForEach';
 export { default as find } from './find';
 export { default as omit } from './omit';
--- packages/utils/test/parseUrl.spec.ts
+++ packages/utils/test/parseUrl.spec.ts
@@ -1,7 +1,105 @@
-import parseUrl from '../src/parseUrl';
+import parseUrl, { Keys } from '../src/parseUrl';
+
+
+const keys: Array<Keys> = [
+  'username',
+  'password',
+  'port',
+  'host',
+  'pathname',
+  'href',
+  'search',
+  'protocol',
+  'hash',
+];
 
 describe('Test parseUrl', () => {
-  it('fullUrl http://hotel.local.alipay.net/123/asdasd?a=1&b[123123]=123#b=2', () => {
-
+  it('fullUrl https://abc:xyz@example.com:8080/123/asdasd?a=1&c=123#b=2', () => {
+    const url = 'https://abc:xyz@example.com:8080/123/asdasd?a=1&c=123#b=2';
+    const parseResult = parseUrl(url);
+    const resultsMap: any = {
+      hash: '#b=2',
+      host: 'example.com',
+      href: url,
+      password: 'xyz',
+      username: 'abc',
+      pathname: '/123/asdasd',
+      port: '8080',
+      protocol: 'https',
+      search: '?a=1&c=123',
+    };
+    keys.forEach((key) => {
+      expect(parseResult[key]).toBe(resultsMap[key] || '');
+    });
+  });
+  it('url https://example.com', () => {
+    const url = 'https://example.com';
+    const parseResult = parseUrl(url);
+    const resultsMap: any = {
+      href: url,
+      host: 'example.com',
+      protocol: 'https',
+    };
+    keys.forEach((key) => {
+      expect(parseResult[key]).toBe(resultsMap[key] || '');
+    });
+  });
+  it('url https://example.com:8080', () => {
+    const url = 'https://example.com:8080';
+    const parseResult = parseUrl(url);
+    const resultsMap: any = {
+      href: url,
+      host: 'example.com',
+      protocol: 'https',
+      port: '8080',
+    };
+    keys.forEach((key) => {
+      expect(parseResult[key]).toBe(resultsMap[key] || '');
+    });
+  });
+  it('url https://example.com:8080/123/123', () => {
+    const url = 'https://example.com:8080/123/123';
+    const parseResult = parseUrl(url);
+    const resultsMap: any = {
+      href: url,
+      host: 'example.com',
+      protocol: 'https',
+      port: '8080',
+      pathname: '/123/123',
+    };
+    keys.forEach((key) => {
+      expect(parseResult[key]).toBe(resultsMap[key] || '');
+    });
+  });
+  it('url https://example.com:8080/123/123?a=1', () => {
+    const url = 'https://example.com:8080/123/123?a=1';
+    const parseResult = parseUrl(url);
+    const resultsMap: any = {
+      href: url,
+      host: 'example.com',
+      protocol: 'https',
+      port: '8080',
+      pathname: '/123/123',
+      search: '?a=1',
+    };
+    keys.forEach((key) => {
+      expect(parseResult[key]).toBe(resultsMap[key] || '');
+    });
+  });
+  it('url https://example.com:8080/123/123?a=1#b=1', () => {
+    const url = 'https://example.com:8080/123/123?a=1#b=1';
+    const parseResult = parseUrl(url);
+    const resultsMap: any = {
+      href: url,
+      host: 'example.com',
+      protocol: 'https',
+      port: '8080',
+      pathname: '/123/123',
+      search: '?a=1',
+      hash: '#b=1',
+    };
+    keys.forEach((key) => {
+      expect(parseResult[key]).toBe(resultsMap[key] || '');
+    });
   });
 });
