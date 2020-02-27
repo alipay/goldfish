fe58ab7f5619a827760782863bc44606446fbf63
yibuyisheng <yibuyisheng@163.com>
2020-02-28 01:02:04 +0800

    feat: export

--- packages/react/babel-plugin-import-config.js
+++ packages/react/babel-plugin-import-config.js
@@ -12,6 +12,13 @@ module.exports = {
       useContextType: `${codeDir}/useContextType`,
       useProps: `${codeDir}/useProps`,
       useState: `${codeDir}/useState`,
+      useFetchInitData: `${codeDir}/useFetchInitData`,
+      useGlobalData: `${codeDir}/useGlobalData`,
+      useGlobalFetchInitData: `${codeDir}/useGlobalFetchInitData`,
+      useGlobalReady: `${codeDir}/useGlobalReady`,
+      useLocalReady: `${codeDir}/useLocalReady`,
+      usePlugins: `${codeDir}/usePlugins`,
+      usePluginsReady: `${codeDir}/usePluginsReady`,
     }[name];
   },
 };
