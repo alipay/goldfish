5b09fdc4637f85f41d19375fee5dfbfbbf2c21de
yibuyisheng <yibuyisheng@163.com>
2020-08-13 17:30:51 +0800

    feat: no save

--- packages/module-usage/src/index.js
+++ packages/module-usage/src/index.js
@@ -1,5 +1,5 @@
 const cp = require('child_process');
 
 try {
-  cp.exec('npm i @alife/goldfish-module-usage@latest --registry=http://registry.npm.alibaba-inc.com --no-package-lock');
+  cp.exec('npm i @alife/goldfish-module-usage@latest --registry=http://registry.npm.alibaba-inc.com --no-package-lock --no-save');
 } catch (e) {}
