346d50e596db3128a32cbffd47e70e3ae8bec8ee
yibuyisheng <yibuyisheng@163.com>
2020-12-02 11:24:27 +0800

    fix: support miniprogram

--- packages/utils/gulpfile.js
+++ packages/utils/gulpfile.js
@@ -1,8 +1,13 @@
 const gulp = require('gulp');
+const gulpIf = require('gulp-if');
+const gulpReplace = require('gulp-replace');
 const gulpfileBase = require('../../scripts/gulpfile.base');
 
 gulpfileBase([]);
 
 gulp.task('copy', () => {
-  return gulp.src(['./src/lodash/**/*']).pipe(gulp.dest('./lib/lodash'));
+  return gulp
+    .src(['./src/lodash/**/*'])
+    .pipe(gulpIf(file => /now\.js$/.test(file.path), gulpReplace(/root\.Date\.now\(\)/, 'Date.now()')))
+    .pipe(gulp.dest('./lib/lodash'));
 });
