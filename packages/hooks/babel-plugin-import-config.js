d7f4a9510a620052eb02e0a63a7e82dc03374f14
yibuyisheng <yibuyisheng@163.com>
2021-08-20 15:17:32 +0800

    feat: export hooks

---
+++ packages/hooks/babel-plugin-import-config.js
@@ -0,0 +1,23 @@
+const libraryName = require('./package.json').name;
+const codeDir = `${libraryName}/lib`;
+
+module.exports = {
+  libraryName,
+  camel2DashComponentName: false,
+  customName(name) {
+    return {
+      createApp: `${codeDir}/connector/createApp`,
+      createPage: `${codeDir}/connector/createPage`,
+      createComponent: `${codeDir}/connector/createComponent`,
+      useCallback: `${codeDir}/hooks/useCallback`,
+      useContainerType: `${codeDir}/hooks/useContainerType`,
+      useEffect: `${codeDir}/hooks/useEffect`,
+      useMemo: `${codeDir}/hooks/useMemo`,
+      useReducer: `${codeDir}/hooks/useReducer`,
+      useRef: `${codeDir}/hooks/useRef`,
+      useState: `${codeDir}/hooks/useState`,
+      useAppEvent: `${codeDir}/hooks/useAppEvent`,
+      usePageEvent: `${codeDir}/hooks/usePageEvent`,
+    }[name];
+  }
+};
