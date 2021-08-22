7cba9a99c12c305390c2c8ea3c45a76d75c7fb89
yibuyisheng <yibuyisheng@163.com>
2021-08-22 17:00:19 +0800

    feat: lerna publish

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,11 +1,3 @@
-const { runCommand, lerna } = require('./utils');
+const { runCommand } = require('./utils');
 
-const packages = lerna.list();
-
-packages.forEach((pkg) => {
-  // Do not release the private packages.
-  if (pkg.private) {
-    return;
-  }
-  runCommand(`cd ${pkg.location} && yarn publish --registry https://registry.npmjs.org --access public`);
-});
+runCommand('lerna publish');
