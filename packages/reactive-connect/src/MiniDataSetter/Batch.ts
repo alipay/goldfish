22b6cdf411b229c49b009f06656b62389c973693
yibuyisheng <yibuyisheng@163.com>
2021-09-24 10:26:02 +0800

    fix: avoid the calling of `set` in the `cb` function synchronously.

--- packages/reactive-connect/src/MiniDataSetter/Batch.ts
+++ packages/reactive-connect/src/MiniDataSetter/Batch.ts
@@ -5,11 +5,17 @@ export default class Batch {
 
   private cb: () => void;
 
+  private isCallingCb = false;
+
   public constructor(cb: () => void) {
     this.cb = cb;
   }
 
   public async set(): Promise<void> {
+    if (this.isCallingCb) {
+      console.warn('Caution: the callback function is currently being called synchronously!');
+    }
+
     const segIndex = this.counter === 0 ? this.segTotalList.length : this.segTotalList.length - 1;
 
     if (!this.segTotalList[segIndex]) {
@@ -28,9 +34,17 @@ export default class Batch {
       const segLength = this.segTotalList.length;
       await Promise.resolve();
       if (this.segTotalList.length === segLength) {
-        this.cb();
         this.counter = 0;
         this.segTotalList = [];
+
+        this.isCallingCb = true;
+        try {
+          this.cb();
+        } catch (e) {
+          throw e;
+        } finally {
+          this.isCallingCb = false;
+        }
       }
     }
   }
