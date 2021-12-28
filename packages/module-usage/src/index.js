1185e229b76602bd381ee08d837d197dea4cab87
yibuyisheng <yibuyisheng@163.com>
2021-12-28 14:00:58 +0800

    chore: print the output message.

--- packages/module-usage/src/index.js
+++ packages/module-usage/src/index.js
@@ -1,5 +1,7 @@
 const cp = require('child_process');
 
 try {
-  cp.exec('npm i @alife/goldfish-module-usage@latest --registry=http://registry.npm.alibaba-inc.com --no-package-lock --no-save');
+  const p = cp.exec('npm i @alife/goldfish-module-usage@latest --registry=http://registry.npm.alibaba-inc.com --no-package-lock --no-save');
+  p.stdout.pipe(process.stdout);
+  p.stderr.pipe(process.stderr);
 } catch (e) {}
