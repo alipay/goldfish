a92fb03873ba9ced781eb8a272867f9ed734f777
yibuyisheng <yibuyisheng@163.com>
2022-02-24 16:49:19 +0800

    feat: optimize the utils.

--- packages/pre-build/src/utils.js
+++ packages/pre-build/src/utils.js
@@ -7,15 +7,15 @@ const lodash = require('lodash');
 const cwd = process.cwd();
 
 function log(...args) {
-  console.log(chalk.bold.green('[Goldfish]'), ...args);
+  console.log(chalk.bold.green('[LOG]'), ...args);
 }
 
 function error(...args) {
-  console.error(chalk.bold.red('[Goldfish]'), ...args);
+  console.error(chalk.bold.red('[ERROR]'), ...args);
 }
 
 function warn(...args) {
-  console.error(chalk.bold.yellow('[Goldfish]'), ...args);
+  console.error(chalk.bold.yellow('[WARN]'), ...args);
 }
 
 exports.log = log;
@@ -25,57 +25,66 @@ exports.error = error;
 exports.warn = warn;
 
 exports.exec = (cmd, options) => {
+  const needInput = options ? !!options.needInput : false;
+  const color = options ? (options.color !== false ? true : false) : true;
+  const prefix = options ? options.prefix : '';
+  const localCwd = (options && options.cwd) || cwd;
+  const env = {
+    ...process.env,
+    ...(options && options.env ? options.env : {}),
+  };
+
   return new Promise((resolve, reject) => {
-    log(`start executing: ${cmd}, cwd: ${(options && options.cwd) || cwd}`);
+    if (prefix) {
+      concurrently(
+        [{ command: `${cmd}${color ? ' --color' : ''}`, cwd: localCwd, name: prefix, env, prefixColor: '#00a0e9' }],
+        {
+          prefix,
+          inputStream: needInput ? process.stdin : undefined,
+        },
+      ).then(resolve, reject);
+    } else {
+      log(`start executing: ${cmd}, cwd: ${localCwd}`);
 
-    let p;
-    try {
-      p = cp.exec(cmd, options);
-    } catch (e) {
-      log(`failed executing: ${cmd}, \n ${e} \n`);
-      throw e;
+      let p;
+      try {
+        p = cp.exec(`${cmd}${color ? ' --color' : ''}`, { env: process.env, ...options, cwd: localCwd });
+      } catch (e) {
+        log(`failed executing: ${cmd}, \n ${e} \n`);
+        throw e;
+      }
+
+      if (needInput) {
+        process.stdin.pipe(p.stdin);
+      }
+
+      p.stdout.pipe(process.stdout);
+      p.stderr.pipe(process.stderr);
+
+      p.on('exit', code => {
+        needInput && process.stdin.unref();
+        log(`complete executing: ${cmd}, with code: ${code}\n`);
+        code === 0 ? resolve() : reject(code);
+      });
     }
-
-    p.stdout.on('data', chunk => {
-      process.stdout.write(chunk);
-    });
-    p.stderr.on('data', chunk => {
-      process.stderr.write(chunk);
-    });
-
-    p.on('exit', code => {
-      log(`complete executing: ${cmd}, with code: ${code}\n`);
-      code === 0 ? resolve() : reject(code);
-    });
   });
 };
 
 const miniProjectConfigFilePath = `${cwd}${path.sep}mini.project.json`;
-const miniProjectConfig = fs.existsSync(miniProjectConfigFilePath)
-  ? require(miniProjectConfigFilePath)
-  : {};
+const miniProjectConfig = fs.existsSync(miniProjectConfigFilePath) ? require(miniProjectConfigFilePath) : {};
 
-const distDir = path.resolve(
-  cwd,
-  process.env.OUT_DIR || lodash.get(miniProjectConfig, 'dist', 'dist')
-);
+const distDir = path.resolve(cwd, process.env.OUT_DIR || lodash.get(miniProjectConfig, 'dist', 'dist'));
 exports.distDir = distDir;
 
 const baseDir = path.resolve(
   cwd,
-  process.env.BASE_DIR ||
-    lodash.get(miniProjectConfig, 'compilerOptions.baseDir', '.')
+  process.env.BASE_DIR || lodash.get(miniProjectConfig, 'compilerOptions.baseDir', '.'),
 );
 exports.baseDir = baseDir;
 
 const tsconfigPath = path.resolve(
   cwd,
-  process.env.TSCONFIG_PATH ||
-    lodash.get(
-      miniProjectConfig,
-      'compilerOptions.tsconfigPath',
-      './tsconfig.json'
-    )
+  process.env.TSCONFIG_PATH || lodash.get(miniProjectConfig, 'compilerOptions.tsconfigPath', './tsconfig.json'),
 );
 exports.tsconfigPath = tsconfigPath;
 
@@ -92,16 +101,17 @@ exports.recordFileUpdateTime = filePath => {
 
   mtimesJson = {
     ...mtimesJson,
-    [filePath]: time
+    [filePath]: time,
   };
   fs.writeJSONSync(cacheFilePath, mtimesJson);
 };
 
 function getCompiledPath(sourceFilePath, sourceType) {
-  const type = sourceType.check(sourceFilePath);
+  const relativeSourcePath = '.' + sourceFilePath.replace(cwd, '');
+  const type = sourceType.check(relativeSourcePath.replace(/^.\//, ''));
   const interTargetPath = path.resolve(
     distDir,
-    '.' + sourceFilePath.replace(cwd, '')
+    exports.isMinifishProject ? relativeSourcePath.replace(new RegExp(`^\\.\\/src\\/`), '') : relativeSourcePath,
   );
 
   if (type === 'ts') {
