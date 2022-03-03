23cb1ce8172fd7f7f1dd9cdb5b9a723544ecad37
yibuyisheng <yibuyisheng@163.com>
2022-03-03 18:59:25 +0800

    feat: use tnpm

--- packages/module-usage/src/index.js
+++ packages/module-usage/src/index.js
@@ -1,7 +1,7 @@
 const cp = require('child_process');
 
 try {
-  const p = cp.exec('npm i @alife/goldfish-module-usage@latest --registry=http://registry.npm.alibaba-inc.com --no-package-lock --no-save');
+  const p = cp.exec('tnpm i @alife/goldfish-module-usage@latest --no-package-lock --no-save');
   p.stdout.pipe(process.stdout);
   p.stderr.pipe(process.stderr);
 } catch (e) {}
