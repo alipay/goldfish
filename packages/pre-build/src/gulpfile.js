c1f014a57db5534265a5bc7ad8dd16b6dc8b4be3
ThomasTrainset <gotoanything@foxmail.com>
2022-09-05 08:05:27 +0000

    feat(pre-build): support STAGE_NAME env name

--- packages/pre-build/src/gulpfile.js
+++ packages/pre-build/src/gulpfile.js
@@ -61,6 +61,7 @@ function commonStream(files, cb) {
     gulp
       .src(files, { base: baseDir })
       .pipe(replace('process.env.NODE_ENV', JSON.stringify(process.env.NODE_ENV)))
+      .pipe(replace('process.env.STAGE_NAME', JSON.stringify(process.env.STAGE_NAME)))
       .pipe(replace('process.env.GOLDFISH_ENV', JSON.stringify(process.env.GOLDFISH_ENV)))
       .pipe(plumber(utils.error)),
   );
