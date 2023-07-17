ccb8d0260aed3614cc53e669d17c427eab073160
yibuyisheng <yibuyisheng@163.com>
2023-07-17 15:01:58 +0800

    feat: release the memory.

--- packages/pre-build/src/findMiniDependencies/fileCache.ts
+++ packages/pre-build/src/findMiniDependencies/fileCache.ts
@@ -1,7 +1,15 @@
+import os from 'os';
 import * as fs from 'fs-extra';
+import sizeof from 'object-sizeof';
+import utils from '../utils';
 
 export class FileCache {
-  private recorder = new Map<string, Map<string, { value: any; lastModifiedTime: number }>>();
+  private recorder = new Map<
+    string,
+    Map<string, { value: any; lastModifiedTime: number; cacheTime: number; cacheSize: number }>
+  >();
+  private minProcessMemo = 1024 * 1024 * 1024 * 8;
+  private minOSMemo = 1024 * 1024 * 1024 * 8;
 
   run<R>(group: string, filePath: string, fn: () => R): R {
     const result = this.recorder.get(group)?.get(filePath);
@@ -12,13 +20,52 @@ export class FileCache {
         this.recorder.set(group, new Map());
       }
       const value = fn();
-      this.recorder.get(group)?.set(filePath, { value, lastModifiedTime });
+      this.recorder
+        .get(group)
+        ?.set(filePath, { value, lastModifiedTime, cacheTime: Date.now(), cacheSize: sizeof(value) });
     }
-    return this.recorder.get(group)?.get(filePath)?.value;
+    const ret = this.recorder.get(group)?.get(filePath)?.value;
+    try {
+      this.releaseOldFiles(this.getMemoToFree());
+    } catch (e) {
+      utils.warn('Release memory failed.', e);
+    }
+    return ret;
   }
 
   clear() {
     this.recorder.clear();
+  }
+
+  private releaseOldFiles(memoToFree: number) {
+    if (memoToFree <= 0) {
+      return;
+    }
+
+    const sortedArr: Array<{ group: string; filePath: string; cacheTime: number; cacheSize: number }> = [];
+    this.recorder.forEach((fileMap, group) => {
+      fileMap.forEach(({ cacheTime, cacheSize }, filePath) => {
+        sortedArr.push({ group, filePath, cacheTime, cacheSize });
+      });
+    });
+    sortedArr.sort((a, b) => a.cacheTime - b.cacheTime);
+
+    let remainedMemoToFree = memoToFree;
+    sortedArr.some(item => {
+      this.recorder.get(item.group)?.delete(item.filePath);
+      remainedMemoToFree -= item.cacheSize;
+      return remainedMemoToFree <= 0;
+    });
+  }
+
+  private getMemoToFree() {
+    const rssMemo = process.memoryUsage.rss();
+    if (rssMemo <= this.minProcessMemo) {
+      return 0;
+    }
+
+    const freeMemo = os.freemem();
+    return freeMemo < this.minOSMemo ? rssMemo - this.minProcessMemo : 0;
   }
 }
 
--- packages/pre-build/src/lib.d.ts
+++ packages/pre-build/src/lib.d.ts
@@ -1,1 +1,5 @@
 declare module 'less-plugin-npm-import';
+declare module 'object-sizeof' {
+  const sizeof: (val: any) => number;
+  export default sizeof;
+}
