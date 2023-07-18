ef0af3263862b8d54393fb5232b03f4128779a89
yibuyisheng <yibuyisheng@163.com>
2023-07-18 16:19:46 +0800

    feat: use timeout

--- packages/pre-build/src/findMiniDependencies/fileCache.ts
+++ packages/pre-build/src/findMiniDependencies/fileCache.ts
@@ -1,17 +1,13 @@
-import os from 'os';
 import * as fs from 'fs-extra';
-import sizeof from 'object-sizeof';
 import utils from '../utils';
 
 export class FileCache {
   private recorder = new Map<
     string,
-    Map<string, { value: any; lastModifiedTime: number; cacheTime: number; cacheSize: number }>
+    Map<string, { value: any; lastModifiedTime: number; cacheExpiredTime?: number }>
   >();
-  private minProcessMemo = 1024 * 1024 * 1024 * 8;
-  private minOSMemo = 1024 * 1024 * 1024 * 8;
 
-  run<R>(group: string, filePath: string, fn: () => R): R {
+  run<R>(group: string, filePath: string, fn: () => R, options?: { cacheTime: number }): R {
     const result = this.recorder.get(group)?.get(filePath);
     const lastModifiedTime = fs.statSync(filePath).mtime.getTime();
     const isExpired = !result || lastModifiedTime > result.lastModifiedTime;
@@ -20,13 +16,13 @@ export class FileCache {
         this.recorder.set(group, new Map());
       }
       const value = fn();
-      this.recorder
-        .get(group)
-        ?.set(filePath, { value, lastModifiedTime, cacheTime: Date.now(), cacheSize: sizeof(value) });
+      const cacheTime = options?.cacheTime;
+      const cacheExpiredTime = !cacheTime || cacheTime <= 0 ? 30 * 60 * 1000 : Date.now() + cacheTime;
+      this.recorder.get(group)?.set(filePath, { value, lastModifiedTime, cacheExpiredTime });
     }
     const ret = this.recorder.get(group)?.get(filePath)?.value;
     try {
-      this.releaseOldFiles(this.getMemoToFree());
+      this.releaseOldFiles();
     } catch (e) {
       utils.warn('Release memory failed.', e);
     }
@@ -37,35 +33,15 @@ export class FileCache {
     this.recorder.clear();
   }
 
-  private releaseOldFiles(memoToFree: number) {
-    if (memoToFree <= 0) {
-      return;
-    }
-
-    const sortedArr: Array<{ group: string; filePath: string; cacheTime: number; cacheSize: number }> = [];
+  private releaseOldFiles() {
     this.recorder.forEach((fileMap, group) => {
-      fileMap.forEach(({ cacheTime, cacheSize }, filePath) => {
-        sortedArr.push({ group, filePath, cacheTime, cacheSize });
+      fileMap.forEach((item, filePath) => {
+        const { cacheExpiredTime } = item;
+        if (cacheExpiredTime && cacheExpiredTime >= Date.now()) {
+          this.recorder.get(group)?.delete(filePath);
+        }
       });
     });
-    sortedArr.sort((a, b) => a.cacheTime - b.cacheTime);
-
-    let remainedMemoToFree = memoToFree;
-    sortedArr.some(item => {
-      this.recorder.get(item.group)?.delete(item.filePath);
-      remainedMemoToFree -= item.cacheSize;
-      return remainedMemoToFree <= 0;
-    });
-  }
-
-  private getMemoToFree() {
-    const rssMemo = process.memoryUsage.rss();
-    if (rssMemo <= this.minProcessMemo) {
-      return 0;
-    }
-
-    const freeMemo = os.freemem();
-    return freeMemo < this.minOSMemo ? rssMemo - this.minProcessMemo : 0;
   }
 }
 
