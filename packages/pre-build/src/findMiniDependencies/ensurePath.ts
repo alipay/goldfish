de65bcaf548af9366e6687f0344e7201a320aef2
ThomasTrainset <gotoanything@foxmail.com>
2023-10-11 16:57:53 +0800

    feat(pre-build): traverse files

--- packages/pre-build/src/findMiniDependencies/ensurePath.ts
+++ packages/pre-build/src/findMiniDependencies/ensurePath.ts
@@ -1,7 +1,16 @@
 import * as fs from 'fs-extra';
 
-export default function ensurePath(p: string) {
+export default function ensurePath(p: string, exts: string[] = []) {
   if (fs.existsSync(p)) {
     return p;
+  }
+
+  if(exts.length) {
+    for(let i = 0; i < exts.length; i++) {
+      const file = p + exts[i]
+        if (fs.existsSync(file)) {
+          return file;
+        }
+    }
   }
 }
--- packages/pre-build/src/findMiniDependencies/findPages.ts
+++ packages/pre-build/src/findMiniDependencies/findPages.ts
@@ -2,6 +2,15 @@ import * as path from 'path';
 import * as fs from 'fs-extra';
 import ensurePath from './ensurePath';
 import fileCache from './fileCache';
+import lodash from 'lodash'
+
+type AppConfig = {
+  pages?: string[];
+  subPackages?: {
+    root: string
+    pages: string[]
+  }[]
+}
 
 /**
  * Find the pages for mini-program.
@@ -11,14 +20,18 @@ import fileCache from './fileCache';
 export default function findPages(projectDir: string) {
   const configFilePath = path.resolve(projectDir, 'app.json');
   return fileCache.run('findPages', configFilePath, () => {
-    const appJson: { pages?: string[] } = fs.readJsonSync(configFilePath);
-    return (appJson.pages || []).map(page => {
+    const { pages = [], subPackages = [] }: AppConfig = fs.readJsonSync(configFilePath);
+    const allPages = lodash.concat(pages, lodash.flatten(subPackages.map(sub => sub.pages.map(page => `${sub.root}/${page}`))))
+
+    return allPages.map(page => {
+      const filePrefix = path.resolve(projectDir, page);
+
       return {
         configPath: page,
         configFilePath,
-        jsPath: path.resolve(projectDir, `${page}.js`),
+        jsPath: ensurePath(filePrefix, ['.js', '.ts']) as string,
         axmlPath: path.resolve(projectDir, `${page}.axml`),
-        acssPath: ensurePath(path.resolve(projectDir, `${page}.acss`)),
+        acssPath: ensurePath(filePrefix, ['.acss', '.less']),
         jsonPath: ensurePath(path.resolve(projectDir, `${page}.json`)),
       };
     });
